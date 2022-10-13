ba72bb3038e038e5119da3ff48ffde1bc8d377e8
Arsenaut <senya.ds.researcher@gmail.com>
2022-10-13 23:05:32 +0300

    mode switcher bug fixes & post hide option

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -8,7 +8,7 @@ Creative Commons Attribution 4.0 International License
 */
 
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
-let theme = sessionStorage.getItem('theme');
+let theme = sessionStorage.getItem('theme') || localStorage.getItem('theme');
 
 const iconSun = "{{ site.baseurl }}/assets/img/sun.svg";
 const iconMoon = "{{ site.baseurl }}/assets/img/moon.svg";
@@ -46,20 +46,20 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc(iconSun);
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc(iconMoon);
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc(iconSun);
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc(iconMoon);
 	}
+    let src = (sessionStorage.getItem('theme') === 'dark') ? iconMoon : iconSun;
+    document.getElementById("theme-toggle-img").src = src;
+    document.getElementById("theme-toggle-img--mobile").src = src;
+    localStorage.setItem('theme', sessionStorage.getItem('theme'));
 }
 
 if (theme === "dark") {
